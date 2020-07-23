c2a84a68e73b9f6d626d03ac2eff7a0f40b1288b
xalejandrow <xalejandrow@gmail.com>
2020-07-23 12:27:16 -0300

    nodejs S1V18 Functional approach

--- node-basic/app.js
+++ node-basic/app.js
@@ -1,26 +1,13 @@
-/* const express = require('express')
-
-const app = express()
- 
-app.get('/', function (req, res) {
-  res.send('Hello World from express')
-}) 
-app.listen(3000) */
-
 const fs = require('fs')
 const fileName = "target.txt"
 
-//fs.watch(fileName, () => console.log(`File changed!`))
+const errHandle = err => console.log(err);
 
-const data = fs.readFileSync(fileName)
-console.log(data.toString());
+const dataHandle = data => console.log(data.toString());
 
 fs.readFile(fileName, (err, data) => {
-  if(err){
-    console.log(err);
-  }else{
-    console.log(data.toString());
-  }
+  if(err) errHandle(err);
+  dataHandle(data);
 });
 
 console.log('Node js async programming...'); 
