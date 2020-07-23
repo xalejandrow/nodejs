4b1ff0541b87dac67a57811892285468e2533a84
xalejandrow <xalejandrow@gmail.com>
2020-07-23 11:23:49 -0300

    S1V13

--- node-basic/app.js
+++ node-basic/app.js
@@ -1,11 +1,9 @@
-const {sum} = require("./helpers");
-const http = require("http");
+const express = require('express')
 
-const server = http.createServer((req, res)=>{
-    res.end(" Hello world from node js real update");
-});
-
-server.listen(3000);
-
-const total = sum(10,200);
-console.log("Total: ", total);
+const app = express()
+ 
+app.get('/', function (req, res) {
+  res.send('Hello World from express')
+})
+ 
+app.listen(3000)
