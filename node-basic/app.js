5ea77713bb5fd6c10f3509f652aa5604a92d5e29
xalejandrow <xalejandrow@gmail.com>
2020-07-23 11:45:12 -0300

    nodejs S1V15 Programming for event loop

--- node-basic/app.js
+++ node-basic/app.js
@@ -1,9 +1,13 @@
-const express = require('express')
+/* const express = require('express')
 
 const app = express()
  
 app.get('/', function (req, res) {
   res.send('Hello World from express')
-})
- 
-app.listen(3000)
+}) 
+app.listen(3000) */
+
+const fs = require('fs')
+const fileName = "target.txt"
+
+fs.watch(fileName, () => console.log(`File changed!`))
