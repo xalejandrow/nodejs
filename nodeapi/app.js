08a2e413c8676fed5c82146ebf0d90a7f8b1d327
xalejandrow <xalejandrow@gmail.com>
2020-07-24 10:14:41 -0300

    Getting Started S2V22 - Middleware explained

--- nodeapi/app.js
+++ nodeapi/app.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 
 //bring in routes
 const {getPosts} = require("./routes/post");
+
+const myOwnMiddleware = (req, res, next) => {
+    console.log("middleware applied!!");
+    next();
+};
+
+//middleware
+app.use(morgan("dev"));
+app.use(myOwnMiddleware);
 
 app.get("/",getPosts);
 
