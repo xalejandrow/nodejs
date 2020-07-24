ec0e6bb7ea5ff59cca0db95f28c0a0f23ce236d0
xalejandrow <xalejandrow@gmail.com>
2020-07-24 09:51:23 -0300

    Getting Started S2V21 - Separating routes

--- nodeapi/app.js
+++ nodeapi/app.js
@@ -1,9 +1,10 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello world from node js");
-})
+//bring in routes
+const {getPosts} = require("./routes/post");
+
+app.get("/",getPosts);
 
 const port = 8080;
 app.listen(port,() => {console.log(`A Node Js API is listening on port: ${port}`)});
