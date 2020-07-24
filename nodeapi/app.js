ad30cdc727deae654e58c9514b6bc9eeea1a4fea
xalejandrow <xalejandrow@gmail.com>
2020-07-24 11:38:07 -0300

    Getting Started S2V26 - Signup with mlab to use mongodb

--- nodeapi/app.js
+++ nodeapi/app.js
@@ -1,6 +1,20 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 const morgan = require("morgan");
+const dotenv = require("dotenv");
+dotenv.config();
+
+// db
+mongoose
+    .connect(
+        process.env.MONGO_URI,
+        { useNewUrlParser: true } )
+    .then( () => console.log("DB Connected"));
+
+    mongoose.connection.on('error', err => {
+        console.log('DB Connection error: ${err.message}');
+})
 
 //bring in routes
 const postRoutes = require("./routes/post");
@@ -16,5 +30,5 @@ app.use(morgan("dev"));
 
 app.use("/",postRoutes);
 
-const port = 8080;
+const port = process.env.PORT || 8080;
 app.listen(port,() => {console.log(`A Node Js API is listening on port: ${port}`)});
