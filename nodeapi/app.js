6c3f20f517200c4bf5e3db9f6b8f776a5a47631d
xalejandrow <xalejandrow@gmail.com>
2020-07-24 10:31:40 -0300

    Getting Started S2V23 - Using controllers

--- nodeapi/app.js
+++ nodeapi/app.js
@@ -3,18 +3,18 @@ const app = express();
 const morgan = require("morgan");
 
 //bring in routes
-const {getPosts} = require("./routes/post");
+const postRoutes = require("./routes/post");
 
-const myOwnMiddleware = (req, res, next) => {
+/* const myOwnMiddleware = (req, res, next) => {
     console.log("middleware applied!!");
     next();
-};
+}; */
 
 //middleware
 app.use(morgan("dev"));
-app.use(myOwnMiddleware);
+//app.use(myOwnMiddleware);
 
-app.get("/",getPosts);
+app.use("/",postRoutes);
 
 const port = 8080;
 app.listen(port,() => {console.log(`A Node Js API is listening on port: ${port}`)});
