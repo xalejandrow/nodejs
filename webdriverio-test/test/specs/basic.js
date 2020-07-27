b40348b2d8aa54f8de602d2c38f96f6dd1a93878
xalejandrow <xalejandrow@gmail.com>
2020-07-27 12:24:20 -0300

    Add Oculow Basic Test

--- webdriverio-test/test/specs/basic.js
+++ webdriverio-test/test/specs/basic.js
@@ -1,6 +1,13 @@
-describe('webdriver.io page', () => {
+/* describe('webdriver.io page', () => {
     it('should have the right title', () => {
         browser.url('https://webdriver.io')
         expect(browser).toHaveTitle('WebdriverIO · Next-gen browser and mobile automation test framework for Node.js');
     })
-})
+}); */
+
+describe('Simple visual validation with oculow.', () => {
+    it('Should be rendered correctly', () => {
+        browser.url('https://lince-232621.firebaseapp.com/shopping')
+    })
+} );
+
