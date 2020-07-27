50a1c2faa0f7237f24f3b272cec7b3e6aacd7ba6
xalejandrow <xalejandrow@gmail.com>
2020-07-27 11:59:56 -0300

    First Test with WebdriverIO

---
+++ oculow_test/test/specs/basic.js
@@ -0,0 +1,5 @@
+describe('Simple visual validation with oculow',() => {
+    if("Should be rendered correctly", () => {
+        BhxBrowser.url("https://lince-2326213firebaseapp.com/shopping")
+    });
+}) 
